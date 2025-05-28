1c7bc3c744b850eecbdd799caf4ac997a68a58df
Melvin Speckamp <doctype.melvin@gmail.com>
2025-05-28 16:06:17 +0200

    add new composer starting point

--- src/routes/compose/+page.svelte
+++ src/routes/compose/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
+    import { onMount } from 'svelte';
+
+    let templateData = {title: 'Empty Template'}
+
+    let showAddExerciseBtn = false
+
+    onMount(() => {
+        const storedData = localStorage.getItem('userTemplate')
+        if (storedData) {
+            const parsedData = JSON.parse(storedData);
+            templateData = parsedData;
+            console.log('Recovered user data')
+        } else {
+            localStorage.setItem('userTemplate', JSON.stringify({
+                title: 'My Template'
+            }) )
+            console.log('Created empty user template')
+        }
+    })
 
 </script>
 
 <section>
-    
     <div class="header">
         <a href="/">⬅️ Back</a>
         <h2>Lightweight Composer</h2>
@@ -13,7 +31,20 @@
         This is a simple tool to create your own workout templates. You can use it to
         quickly generate workouts based on your preferences.
     </p>
+    <div>
+        <h2 class="template-title" contenteditable="true" on:blur={((e) => {
+            console.log(e.target.textContent)
+            })}>
+            {templateData.title}
+        </h2>
+        <button type="button" class="button-session">
+            Add Session
+        </button>
+       
+    </div>
 </section>
+
+
 
 <style>
     section {
@@ -30,5 +61,27 @@
         align-items: center;
         width: 100%;
         justify-content: space-between;
+    }
+
+    .button-session {
+        background-color: #00c9b2;
+        color: #c2fcf3;
+        border: none;
+        padding: 0.5rem 1rem;
+        display: block;
+        margin: 0 auto;
+    }
+
+    .button-session:hover {
+        background-color: #00a99f;
+        cursor: pointer;
+    }
+
+    .template-title {
+        margin: 1rem;
+        text-align: center;
+        background-color: #6e0e4d;
+        padding: 1rem 2rem;
+
     }
 </style>
